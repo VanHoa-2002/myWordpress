ae8a3eefc537b2587f333bbf315c5f84e7d9d310
VanHoa-2002 <89292546+VanHoa-2002@users.noreply.github.com>
2021-09-16 10:59:41 +0700

    up

--- wp-login.php
+++ wp-login.php
@@ -90,7 +90,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 <script type="text/javascript" src="https://vanhoa-2002.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"8a868b2af7"};
+var userProfileL10n = {"user_id":"0","nonce":"146cb43c20"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
